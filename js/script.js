145a9def6eae71696174986decdcb1cc7127d968
mypaxpax <thomasgade@gmail.com>
2022-04-29 17:31:53 +0200

    Loop for creating squares == to user input.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-let test = document.querySelectorAll(".grid");
+let inside = document.querySelector("#inside");
+let myInput = prompt("Enter number");
 
+for(let i = 0; i < myInput; i++){
+    let myGrid = document.createElement("div");
+    myGrid.textContent = ".";
+    myGrid.classList.add("grid");
+    inside.appendChild(myGrid);
+
+}
+let test = document.querySelectorAll(".grid");
 let paint = function(){
     test.forEach(function(square){
         square.addEventListener("mouseover", () =>{
