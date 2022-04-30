817b5def163abcfd395324a9d11098ada24a154d
mypaxpax <thomasgade@gmail.com>
2022-04-30 16:15:25 +0200

    Create new grid size function button.

--- js/script.js
+++ js/script.js
@@ -1,36 +1,42 @@
 const container = document.querySelector(".container");
-const resetButton = document.querySelector("#reset");
+const clearButton = document.querySelector("#clearButton");
+const newGridSize = document.querySelector("#newGridSize");
 
-// takes user input from prompt window.
-let myInput = prompt("Enter number");
-
-// Error message if more than allowed or empty.
-if (myInput >= 10000 || myInput == null){
-    alert("Too big dude!");
-}
-
+newGridSize.addEventListener("click", gridSize)
 // Creates rows from user input and adds to container as child element.
-for(let i = 0; i < myInput; i++){
-    let row = document.createElement("div");
-    container.appendChild(row);
+function create(userInput){
+    for(let i = 0; i < userInput; i++){
+        let row = document.createElement("div");
+        container.appendChild(row);
 
 // Fills the row with same amount as input, creating 1x1 ration.
-    for (let j = 0; j < myInput; j++){
+    for (let j = 0; j < userInput; j++){
         let squares = document.createElement("div");
         squares.className = "gridSquare";
-        squares.style.width = `${960 / myInput}px`;
-        squares.style.height = `${500 / myInput}px`;
+        squares.style.width = `${960 / userInput}px`;
+        squares.style.height = `${500 / userInput}px`;
         container.appendChild(squares);
+        }
     }
-
 }
 
-// grabs .gridSquare class and assign it to variable
-let paintSquares = document.querySelectorAll(".gridSquare");
+
+// takes input and pass it towards create function.
+function gridSize(){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    userInput = prompt("Enter number");
+    if (userInput >= 100 || userInput == 0){
+        alert("Enter a number below 100");
+        gridSize();
+    }else {
+        create(userInput);
+    }
 
 // runs through each scare and on mouseOver, change class -
 // - to colored square.
-let paint = function(){
+    let paintSquares = document.querySelectorAll(".gridSquare");
     paintSquares.forEach(function(colorSquare){
         colorSquare.addEventListener("mouseover", () =>{
             colorSquare.classList.add("gridSquareColor");
@@ -39,10 +45,10 @@ let paint = function(){
 }
 
 // Clears all created divs.
-resetButton.addEventListener("click", () => {
+clearButton.addEventListener("click", () => {
     while(container.firstChild){
         container.removeChild(container.firstChild);
     }
-});
+})
 
-paint();
+gridSize();
