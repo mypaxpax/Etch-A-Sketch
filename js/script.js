15ac758a2b31607daaf4f0b4b0f336c5a4003e65
mypaxpax <thomasgade@gmail.com>
2022-05-02 12:34:11 +0200

    Reset grid for colored squares.

--- js/script.js
+++ js/script.js
@@ -1,24 +1,27 @@
 const container = document.querySelector(".container");
 const clearButton = document.querySelector("#clearButton");
 const newGridSize = document.querySelector("#newGridSize");
+const clearChildren = document.querySelector(".container").childNodes;
 
-newGridSize.addEventListener("click", gridSize)
-
-// Clears all created divs.
-clearButton.addEventListener("click", () => {
-    while(container.firstChild){
-        container.removeChild(container.firstChild);
+// Clears the colored squares.
+clearAllGrid = () => {
+    for(let k = 0; k < clearChildren.length; k++){
+        if (clearChildren[k].classList){
+            clearChildren[k].classList.remove("gridSquareColor")
+        }
     }
-})
+}
+
 // Creates rows from user input and adds to container as child element.
 create = (userInput) => {
     for(let i = 0; i < userInput; i++){
         let row = document.createElement("div");
         container.appendChild(row);
 
+
 // Fills the row with same amount as input, creating 1x1 ration.
     for (let j = 0; j < userInput; j++){
-        let squares = document.createElement("div");
+        squares = document.createElement("div");
         squares.className = "gridSquare";
         squares.style.width = `${500 / userInput}px`;
         squares.style.height = `${500 / userInput}px`;
@@ -31,8 +34,8 @@ create = (userInput) => {
 // takes input and pass it towards create function.
 function gridSize(){
     while(container.firstChild){
-        container.removeChild(container.firstChild);
-    }
+        container.removeChild(container.firstChild)
+    } 
     userInput = prompt("Enter number");
     if (userInput >= 100 || userInput == 0){
         alert("Enter a number below 100");
@@ -50,5 +53,8 @@ function gridSize(){
         })
     })
 }
+
+newGridSize.addEventListener("click", gridSize)
+clearButton.addEventListener("click", clearAllGrid);
 
 gridSize();
